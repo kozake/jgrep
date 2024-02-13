aa803bd9405494cd4c2b7252fbe9e847f78890af
kozake <sh.kozake@gmail.com>
2024-02-14 00:58:53 +0900

    L&F をNimbusに

--- src/jgrep/Main.java
+++ src/jgrep/Main.java
@@ -2,9 +2,21 @@ package jgrep;
 
 import jgrep.ui.MainFrame;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // If Nimbus is not available, you can set the GUI to another look and feel.
+        }
         MainFrame main = new MainFrame();
     }
 }
